7aa1b30e61a05f0355746e510bf27e9c39dfffaa
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-02-21 16:28:22 +0100

    Update crackme02.js

--- crackme02.js
+++ crackme02.js
@@ -1,5 +1,5 @@
 // Vous devez assigner la bonne string à la variable password ci-dessous
-// Et obtenir le message 'OK' en éxecutant le programme
+// Et obtenir le message 'GOOD' en éxecutant le programme
 
 /* Any letter of the alphabet works as long as there is only one.
 By default I entered B because in the Caesar code, the letter corresponds to the number 1 (without shift key) */
