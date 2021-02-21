8c47b0d5c67451cb1d99f5397269509e6c431996
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-02-22 00:57:31 +0100

    Done

---
+++ crackme10.js
@@ -0,0 +1,28 @@
+// Vous devez assigner la bonne string à la variable password ci-dessous
+// Et obtenir le message 'OK' en éxecutant le programme
+
+let password = "A"; // BINBINBIN = 651
+
+const crackme10 = (password) => {
+  if (
+    password
+      .trim()
+      .toLowerCase()
+      .split("")
+      .filter((elem) => ["a", "e", "i", "o", "u", "y"].includes(elem))
+      .map((elem) => elem.charCodeAt())
+      .reduce((a, b) => {
+        return a + b;
+      }, 0) %
+      2 !==
+    0
+  ) {
+    console.log("OK");
+  } else {
+    console.log("BAD");
+  }
+};
+
+crackme10(password);
+
+// Output : OK
