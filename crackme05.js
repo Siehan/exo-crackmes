83bbaa4862a6f125180d1f1cb5f05c7031372703
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-02-21 16:30:55 +0100

    Update crackme05.js

--- crackme05.js
+++ crackme05.js
@@ -1,5 +1,5 @@
 // Vous devez assigner la bonne string à la variable password ci-dessous
-// Et obtenir le message 'OK' en éxecutant le programme
+// Et obtenir le message 'GOOD' en éxecutant le programme
 
 let password = "4321drowssap";
 
